f68c771bc340c00aacd3efab312934fbf91d3267
Vu Van Hieu <HieuVV@ltsgroup.tech>
2022-10-25 15:52:37 +0700

    bai 59

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,12 @@
 module.exports = function override(config, evn){
     return config;
 }
+const {
+    override,useBabelRc
+  } = require("customize-cra");
+
+  
+  module.exports = override(
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useBabelRc()
+  );
